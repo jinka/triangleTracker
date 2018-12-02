42f2adf1e30af6f79782cb4dcabc101f05320a24
dayud mohamed <voiphandy@gmail.com>
2018-12-02 23:12:45 +0300

    Done Triangle Tracker script and checked

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ function triangleTracker() {
   var isSide2 = false;
   var isSide3 = false;
 
-  alert("Am in js funtion");
   var triangleTypes = ['Equilateral','isosceles','Scalene','Not Triangle'];
 
   var sideLength1 = document.getElementById("id1").value;
@@ -13,30 +12,26 @@ function triangleTracker() {
       document.getElementById("triangleTriangleSide1").innerHTML = side1Obj.validationMessage;
       isSide1=false;
   } else {
-      document.getElementById("triangleTriangleSide1").innerHTML = "side1Obj Input OK";
       isSide1=true;
   }
 
   var sideLength2 = document.getElementById("id2").value;
   var side2Obj = document.getElementById("id2");
   if (!side2Obj.checkValidity()) {
-      document.getElementById("triangleTriangleSide2").innerHTML = side2Obj.validationMessage;
-      isSide2=true;
+      isSide2=false;
   } else {
-    document.getElementById("triangleTriangleSide2").innerHTML = "side2Obj Input OK";
-    isSide2=false;
+    isSide2=true;
   }
 
   var sideLength3 = document.getElementById("id3").value;
   var side3Obj = document.getElementById("id3");
   if (!side3Obj.checkValidity()) {
-      document.getElementById("triangleTriangleSide3").innerHTML = side3Obj.validationMessage;
-      isSide3=true;
+      isSide3=false;
   } else {
-    document.getElementById("triangleTriangleSide3").innerHTML = "side3Obj Input OK";
-    isSide3=false;
+    isSide3=true;
   }
-  if (isSide1 && isSide2 && isSide3) {
+
+  if (isSide1) {
     sideLength1 = parseInt(sideLength1);
     sideLength2 = parseInt(sideLength2);
     sideLength3 = parseInt(sideLength3);
@@ -54,8 +49,9 @@ function triangleTracker() {
           return triangleTypes[1];
         } else if (side1 !== side2 && side1 !== side3 && side2 !== side3) {
           return triangleTypes[2];
+        }
     }
-
   }
-
+  document.getElementById("triangleTriangleSide2").innerHTML = isTriangle(sideLength1,sideLength2,sideLength3);
+}
 }
