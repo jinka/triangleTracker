41acbc15130c2b476e20b48e49c7fa63479d0d07
dayud mohamed <voiphandy@gmail.com>
2018-12-02 22:15:33 +0300

    Checking if user input is ok or not using if statememnt

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
-var isSide1 = false;
-var isSide2 = false;
-var isSide3 = false;
-
 function triangleTracker() {
+  var isSide1 = false;
+  var isSide2 = false;
+  var isSide3 = false;
+
+  alert("Am in js funtion");
   var triangleTypes = ['Equilateral','isosceles','Scalene','Not Triangle'];
 
   var sideLength1 = document.getElementById("id1").value;
@@ -34,6 +35,9 @@ function triangleTracker() {
   } else {
     document.getElementById("triangleTriangleSide3").innerHTML = "side3Obj Input OK";
     isSide3=false;
+  }
+  if (isSide1 && isSide2 && isSide3) {
+    
   }
 
 }
