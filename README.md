20d68e556347026e9fc2da7a30301c2c4b4bc414
dayud mohamed <voiphandy@gmail.com>
2018-12-05 06:50:12 +0300

    Add indentation for all files.

--- README.md
+++ README.md
@@ -1,17 +1,28 @@
 # Project Title
-  ## Triangle Tracker
+
+## Triangle Tracker
+
   A triangle tracker, is a web application app which checks if the user input all three sides then is it a triangle or not.  
+
 # Project Description
+
 Triangle Tracker is useful for who needs to know if the three sides can fit all requirements for a real triangle.
+
 # Getting started
+
  First you see the home page of this week 2 project. To access the triangle tracker you click triangle tracker under project 2.
- To run the project you need to put following link in any browser https://jinka.github.io/triangleTracker/ 
+ To run the project you need to put following link in any browser <https://jinka.github.io/triangleTracker/>
+
 # Author Information
+
 My name is Dayud Mohamed Farah, I am one the Moringa prep class 14.
+
 # Setup Instruction
+
 For setup the app you need to install only a browser. This means the app no installation or setup needed.
 
 # BDD
+
 Triangle Tracking App solves to output if data user input right outputs if is triangle or not. Following are the requirement for the user input:
 a. Not empty data for all the three fields
 b. Must be a numeric.
@@ -28,15 +39,18 @@ c. Scalene Triangle
 After all I will display the result.
 
 # Technologies Used
+
 I have used to build this app following Technologies:
-1. html
-2. css
-3. Javascript
+1\. html
+2\. css
+3\. Javascript
 
 # Contact Information
+
 Dayud Mohamed Farah
 Tel.: +254 716 761 606
 Email: voiphandy@gmail.company
 
 # License and Copyright Information
+
 This app is licensed under the terms of MIT, an Copyright by Moringa School prep class 14.
--- js/script.js
+++ js/script.js
@@ -3,32 +3,32 @@ function triangleTracker() {
   var isSide2 = false;
   var isSide3 = false;
 
-  var triangleTypes = ['Equilateral','isosceles','Scalene','Not a Triangle'];
+  var triangleTypes = ['Equilateral', 'isosceles', 'Scalene', 'Not a Triangle'];
 
   var sideLength1 = document.getElementById("id1").value;
   var side1Obj = document.getElementById("id1");
 
   if (!side1Obj.checkValidity()) {
-      document.getElementById("triangleTriangleSide1").innerHTML = side1Obj.validationMessage;
-      isSide1=false;
+    document.getElementById("triangleTriangleSide1").innerHTML = side1Obj.validationMessage;
+    isSide1 = false;
   } else {
-      isSide1=true;
+    isSide1 = true;
   }
 
   var sideLength2 = document.getElementById("id2").value;
   var side2Obj = document.getElementById("id2");
   if (!side2Obj.checkValidity()) {
-      isSide2=false;
+    isSide2 = false;
   } else {
-    isSide2=true;
+    isSide2 = true;
   }
 
   var sideLength3 = document.getElementById("id3").value;
   var side3Obj = document.getElementById("id3");
   if (!side3Obj.checkValidity()) {
-      isSide3=false;
+    isSide3 = false;
   } else {
-    isSide3=true;
+    isSide3 = true;
   }
 
   if (isSide1) {
@@ -40,19 +40,19 @@ function triangleTracker() {
     var sideOneThree = sideLength1 + sideLength3;
     var sideTwoThree = sideLength2 + sideLength3;
 
-    function isTriangle(side1,side2,side3) {
+    function isTriangle(side1, side2, side3) {
       if (sideOneTwo <= side3 || sideOneThree <= side2 || sideTwoThree <= side1) {
         return triangleTypes[3];
       } else {
-        if (side1==side2 && side1==side3) {
+        if (side1 == side2 && side1 == side3) {
           return triangleTypes[0];
-        }  else if (side1 == side2 || side1 == side3 || side2 == side3) {
+        } else if (side1 == side2 || side1 == side3 || side2 == side3) {
           return triangleTypes[1];
         } else if (side1 !== side2 && side1 !== side3 && side2 !== side3) {
           return triangleTypes[2];
         }
+      }
     }
+    document.getElementById("triangleTriangleSide2").innerHTML = "Triangle is : " + isTriangle(sideLength1, sideLength2, sideLength3);
   }
-  document.getElementById("triangleTriangleSide2").innerHTML = "Triangle is : " + isTriangle(sideLength1,sideLength2,sideLength3);
-}
 }
